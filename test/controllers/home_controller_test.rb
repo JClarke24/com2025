0159436cd18d731dd141f4aa8c9b282824adf1a6
Jonathan Clarke <jc01663@surrey.ac.uk>
2019-11-13 14:34:55 +0000

    Added home page test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'My Gallery'
+    assert_select 'h1', 'My Gallery'
+    assert_select 'h2', 'Welcome to My Gallery, Where all your favorite memories can be together...'
   end
 
 end
